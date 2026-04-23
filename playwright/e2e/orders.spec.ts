346a11f684f3ddc25b6d3006c9145228c5daa0e7
Johnny Dutra <johnny.dutra@gmail.com>
2026-04-23 08:03:52 -0300

    through section 2

--- playwright/e2e/orders.spec.ts
+++ playwright/e2e/orders.spec.ts
@@ -1,22 +1,95 @@
 import { test, expect } from '@playwright/test';
+import { generateOrderCode } from '../support/helpers';
 
-test('should look up an approved order', async ({ page }) => {
-    const orderLookupCode = 'VLO-FBDKHQ';
+test.describe('Order Lookup', () => {
 
-    // arrange
-    await page.goto('http://localhost:5173/');
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173/');
+        await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+        await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+        await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    });
 
-    // act
-    await page.getByLabel('Número do Pedido').fill(orderLookupCode);
-    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+    test('should look up an approved order', async ({ page }) => {
+        // arrange
+        const orderLookupCode = 'VLO-FBDKHQ';
+        
+        // act
+        await page.getByLabel('Número do Pedido').fill(orderLookupCode);
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+        
+        // assert
+        // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10000 });
+        // await expect(page.getByTestId('order-result-id')).toContainText(orderLookupCode);
+        // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+        await expect(page.locator('//p[text()="Pedido"]/following-sibling::p')).toContainText(orderLookupCode);
+        await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible();
+    });
+
+    test('should display an error message when the order is not found', async ({ page }) => {
+        // arrange
+        const orderLookupCode = generateOrderCode();
     
-    // assert
-    // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10000 });
-    // await expect(page.getByTestId('order-result-id')).toContainText(orderLookupCode);
-    // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-    await expect(page.locator('//p[text()="Pedido"]/following-sibling::p')).toContainText(orderLookupCode);
-    await expect(page.locator('//div[text()="APROVADO"]')).toBeVisible();
+        // act
+        await page.getByLabel('Número do Pedido').fill(orderLookupCode);
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+    
+        // assert
+        // const title = page.getByRole('heading', { name: 'Pedido não encontrado' });
+        // const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' });
+    
+        // await expect(title).toBeVisible();
+        // await expect(message).toBeVisible();
+    
+        await expect(page.locator('#root')).toMatchAriaSnapshot(`
+            - img
+            - heading "Pedido não encontrado" [level=3]
+            - paragraph: "Verifique o número do pedido e tente novamente"
+            `);
+    });
+
+    test('should use snapshots to validate order', async ({ page }) => {
+        // arrange
+        const orderLookupCode = 'VLO-FBDKHQ';
+        const targetTestId = 'order-result-' + orderLookupCode;
+        
+        // act
+        await page.getByLabel('Número do Pedido').fill(orderLookupCode);
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+        // assert
+        await expect(page.getByTestId(targetTestId)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${orderLookupCode}
+            - img
+            - text: APROVADO
+            `);
+
+          await expect(page.getByTestId(targetTestId)).toMatchAriaSnapshot(`
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Glacier Blue
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: aero Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Johnny Test
+            - paragraph: Email
+            - paragraph: test@test.com
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+    })
 });
+
+
---
+++ playwright/support/helpers.ts
@@ -0,0 +1,13 @@
+export function generateOrderCode(): string {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const prefix = 'VLO-';
+
+    let randomized = '';
+
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        randomized += chars[randomIndex];
+    }
+
+    return prefix + randomized;
+}
